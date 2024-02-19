27191b4e00dd9a580f769793eddfaab277f9cdc1
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-02-20 03:41:28 +0900

    로그인 라우터 추가

--- app.js
+++ app.js
@@ -5,18 +5,11 @@ require('dotenv').config();
 const app = require('./_initSetting')
 const port = 3000;
 
-// 라우팅 함수를 미들웨어로 처리
-// 미들웨어 (Middleware)는 Express 애플리케이션에서 요청(request)과 응답(response) 사이에 위치하여
-// 요청에 대한 처리를 수행하고 응답을 생성하는 함수
+// 라우팅 함수를 미들웨어(Middleware)로 처리
 app.use('/', require('./routes/index'));
-// app.use('/addComment', require('./routes/addComment'));
+app.use('/login', require('./routes/login'));
+// app.use('/map', require('./routes/map'));
 // app.use('/boardMain', require('./routes/boardMain'));
-// app.use('/create', require('./routes/create'));
-// app.use('/deletePost', require('./routes/deletePost'));
-// app.use('/detailPost', require('./routes/detailPost'));
-// app.use('/editPost', require('./routes/editPost'));
-// app.use('/login', require('./routes/login'));
-// app.use('/loginFail', require('./routes/loginFail'));
 // app.use('/logout', require('./routes/logout'));
 
 // 게시판 서버 시작
