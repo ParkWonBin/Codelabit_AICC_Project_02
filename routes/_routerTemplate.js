dd530a6f88294149bfb8ca14aa99729f460fe030
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-02-19 20:52:04 +0900

    초기설정, 중간저장

---
+++ routes/_routerTemplate.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const oracledb = require('oracledb');
+// const dbConfig = require('../_dbConfig');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const oracledb = require('oracledb');
+// const dbConfig = require('../_dbConfig');
+
+const router = express.Router();
+
+// static 경로를 사용하고 있기 때문에,
+// router.get('/', ...)보다 static 경로의 index.html 이 우선순위가 높습니다.
+
+// post 로 특정 데이터와 함께 진입한 경우, ejs를 랜더링 합니다.
+router.post('/',  (req, res) => {
+    res.render('index',{})
+});
+
+module.exports = router;
