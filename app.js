c4562cc5cc9b46844ffd9ac450bdd7b5ce081e54
junghoon0904 <153577771+junghoon0904@users.noreply.github.com>
2024-02-20 19:01:49 +0900

    로그인 기능 추가

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const port = 3000;
 // 라우팅 함수를 미들웨어(Middleware)로 처리
 app.use('/', require('./routes/index'));
 app.use('/login', require('./routes/login'));
+app.use('/userCreate', require('./routes/userCreate'));
 // app.use('/map', require('./routes/map'));
 // app.use('/boardMain', require('./routes/boardMain'));
 // app.use('/logout', require('./routes/logout'));
 
 // 게시판 서버 시작
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}/login`);
+
 });
