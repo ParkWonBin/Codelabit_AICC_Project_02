98a26110dcb5b8e52b77883a9bc5a3ee187fd6c5
junghoon0904 <153577771+junghoon0904@users.noreply.github.com>
2024-02-20 19:01:16 +0900

    login 파일 수정

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path')
-// const oracledb = require('oracledb');
-// const dbConfig = require('../_dbConfig');
+const path = require('path');
+const oracledb = require('oracledb');
+const dbConfig = require('../_dbConfig');
 
 const router = express.Router();
 
@@ -10,6 +10,29 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    const { userId, userPw } = req.body;
+    console.log('username '+userId)
+    console.log('password '+userPw)
+
+    let connection;
+    try{
+        connection = await oracledb.getConnection(dbConfig);
+        const sql_query = 'select * from users where user_id = :userId and user_pw = :userPw';
+        const result =  await connection.execute(sql_query, {userId, userPw});
+
+       console.log(result.rows)
+        if(result.rows.length > 0){
+            console.log("로그인 성공")
+            res.render('login',{userId:userId})
+        }else {
+            console.log("로그인 실패")
+            res.render('loginFail',{})
+        }
+
+    }catch (e){
+        console.log("오류발생 ㅠㅠ" + e)
+    }finally {
+    }
 
 });
 
