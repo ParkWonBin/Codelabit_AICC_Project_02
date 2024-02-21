57892bce48fdc0a67822e69db32e1f5977518c26
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-02-21 11:20:35 +0900

    라우터 템플릿에 DB연결과 관련된 함수 골격 만들어 넣기

--- routes/_routerTemplate.js
+++ routes/_routerTemplate.js
@@ -13,4 +13,33 @@ router.post('/', async (req, res) => {
 
 });
 
+// DB 연결이 필요한 경우 아래 함수 수정하여 적용
+// async function customFunc(){
+//     let connection;
+//     try {
+//         // DB 네트워크 상태가 안좋으면 connection 만드는 데부터 에러나므로 Try 내부에 넣음.
+//         connection = await oracledb.getConnection(dbConfig);
+//
+//         // sql 쿼리 만들기
+//         const sql_string =  '';
+//         const result = await connection.execute( sql_string,{ } );
+//
+//         // 결과 있으면 리턴값 만들어 리턴하기
+//         if(result){
+//             return result;
+//         }else{
+//             return null;
+//         }
+//     } catch (error) {
+//         console.error('오류 발생:', error);
+//         return null;
+//     } finally {
+//         // 연결정보 남아있으면 close
+//         if (connection) {
+//             await connection.close();
+//         }
+//     }
+// }
+
+
 module.exports = router;
