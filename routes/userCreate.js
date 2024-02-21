074d8872b2cb16a7dd73203a4271b7baa09debfc
junghoon0904 <153577771+junghoon0904@users.noreply.github.com>
2024-02-21 10:12:25 +0900

    회원가입 기능을 완성했습니다.
    db연결이 중간에 끊긴 경우에는 아직 화면 연결을 하지 않았어요.
    추후에 서버장애 안내페이지를 만들게 된다면 연결이 필요합니다.

--- routes/userCreate.js
+++ routes/userCreate.js
@@ -22,8 +22,20 @@ router.post('/', async (req, res) => {
         const result =  await connection.execute(sql_query, {userId, userPw, userName});
 
         console.log(result)
+        console.log("가입이 완료 됐습니다. 자동으로 로그인 합니다.")
+        res.render('login',{userId:userId})
     }catch (e){
         console.log("오류발생 ㅠㅠ" + e)
+
+        if(e.message.includes("unique constraint")){
+            console.log("아이디 혹은 닉네임이 중복된 경우입니다.")
+            res.render('userCreate',{userId, userName})
+
+        }else {
+            console.log("아마도 db 네트워크 상태가 안좋을지도..?")
+           //TODO : 네트워크가 안좋은 경우 여기로 옵니다. 추후에 안내 화면으로 이동시켜주세요.
+        }
+
     }finally {
     }
 });
