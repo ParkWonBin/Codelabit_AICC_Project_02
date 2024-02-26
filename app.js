cba503debe703e8a87ea0edd1a3f166c993f7f4c
Choi Wonnho <wonnho@nate.com>
2024-02-26 21:17:40 +0900

    index.ejs

--- app.js
+++ app.js
@@ -10,9 +10,42 @@ app.use('/', require('./routes/index'));
 app.use('/login', require('./routes/login'));
 app.use('/userCreate', require('./routes/userCreate'));
 app.use('/map', require('./routes/map'));
-app.use('/mapImageUpload', require('./routes/mapImageUpload'));
-// app.use('/boardMain', require('./routes/boardMain'));
+//app.use('/mapImageUpload', require('./routes/mapImageUpload'));
+//app.use('/bulletin', require('./routes/bulletin'));
 // app.use('/logout', require('./routes/logout'));
+
+
+
+
+app.get('/', async (req, res) => {
+    let conn;
+    try {
+       conn = await oracledb.getConnection(dbConfig);
+        let info_bulletin=await conn.execute(
+           `SELECT  writer, title, to_char(created_at,'YYYY-MM-DD'), views
+                FROM (
+                    SELECT  b.title, b.writer_id AS writer, b.created_at, b.views,
+                            ROW_NUMBER() OVER (ORDER BY p.created_at DESC) AS rn
+                    FROM bulletin b
+                    JOIN members m ON b.writer_id = m.member_id
+                    )
+             WHERE rn BETWEEN 1 AND 10`);
+
+        res.render('index',{bulletin:info_bulletin.rows});
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        if (conn) {
+            try {
+                await conn.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
 
 // 게시판 서버 시작
 app.listen(port, () => {
