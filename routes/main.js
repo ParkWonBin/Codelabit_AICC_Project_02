c2495215e5cc9d8be00546c1345a366165a79a40
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-03-01 15:04:25 +0900

    메인페이지 관련 css 마무리.
    안이쁘지만 어쩔 수 없음.

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,7 @@ const express = require('express');
  * */
 const router = express.Router();
 
+// http://localhost:3000/main
 router.get('/', (req, res) => {
     // 로그인 여부에 따라 페이지 보여주기
     if(req.session.userId){
@@ -31,6 +32,7 @@ router.get('/', (req, res) => {
     }
 });
 
+// http://localhost:3000/main/mypage
 router.get('/mypage', (req, res) => {
     res.render('mainLayout', {
         title: '회원정보 페이지 입니다.',
@@ -42,6 +44,7 @@ router.get('/mypage', (req, res) => {
     })
 });
 
+// http://localhost:3000/main/login
 router.get('/login',(req,res)=>{
     res.render('mainForm', {
         title: '로그인 페이지 입니다',
@@ -58,6 +61,7 @@ router.get('/login',(req,res)=>{
     })
 })
 
+// http://localhost:3000/main/CreateUser
 router.get('/CreateUser',(req,res)=>{
     res.render('mainForm', {
         title: '회원가입 페이지 입니다',
@@ -75,14 +79,16 @@ router.get('/CreateUser',(req,res)=>{
     })
 })
 
+// http://localhost:3000/main/UpdateUser
 router.get('/UpdateUser',(req,res)=>{
     res.render('mainForm', {
-        title: '비밀번호 변경 페이지 입니다',
+        title: '비밀번호를 변경합니다',
         action: '/user',
-        method: 'UPDATE',
+        method: 'PATCH',
         inputs:[
-            {name:'userPw', text:'비밀번호', required:true},
-            {name:'userPwConform', text:'비밀번호확인', required:true}
+            {name:'userPwPrev', text:'현재 비밀번호', required:true},
+            {name:'userPwNew', text:'새 비밀번호', required:true},
+            {name:'userPwNewConform', text:'새 비밀번호 확인', required:true}
         ],
         buttons:[
             {tagName:'a', attr:`href=/main`, text:'취소'},
@@ -91,6 +97,7 @@ router.get('/UpdateUser',(req,res)=>{
     })
 })
 
+// http://localhost:3000/main/DeleteUser
 router.get('/DeleteUser',(req,res)=>{
     res.render('mainForm', {
         title: '회원탈퇴 페이지 입니다',
